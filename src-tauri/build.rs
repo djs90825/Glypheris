ccd3e906156c7653318dad1cedd0274a381c3e97
djs90825 <djs90825@gmail.com>
2026-06-13 01:23:28 +0100

    feat: remove prost-build dependency and related proto compilation from build script; add uuid and base64 dependencies

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,14 +1,4 @@
 fn main() {
-    let mut prost_build = prost_build::Config::new();
-    prost_build.out_dir("gen");
-    prost_build.compile_protos(
-        &[
-            "proto/gesture_command.proto",
-            "proto/execution_plan.proto",
-            "proto/inference_packet.proto",
-        ],
-        &["proto/"],
-    ).unwrap_or_else(|e| panic!("Failed to compile protos: {}", e));
-
     tauri_build::build()
 }
+
